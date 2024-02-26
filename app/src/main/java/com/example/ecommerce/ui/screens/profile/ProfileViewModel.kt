b056a6ddd872a87f31de637e4dbe8dc1b28e69f5
Anvitha514421 <149704539+Anvitha514421@users.noreply.github.com>
2024-02-26 00:25:26 +0000

    Added profile screen

---
+++ app/src/main/java/com/example/ecommerce/ui/screens/profile/ProfileViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.ecommerce.ui.screens.profile
+
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ecommerce.common.ScreenState
+import com.example.ecommerce.domain.entity.user.UserInformationEntity
+import com.example.ecommerce.domain.mapper.ProductBaseMapper
+import com.example.ecommerce.domain.usecase.user.read_user.ReadFirebaseUserInfosUseCase
+import com.example.ecommerce.ui.uiData.UserInformationUiData
+import com.example.ecommerce.utils.getUserIdFromSharedPref
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val readFirebaseUserInfosUseCase: ReadFirebaseUserInfosUseCase,
+    private val sharedPreferences: SharedPreferences,
+    private val firebaseUserInfoToUiData: ProductBaseMapper<UserInformationEntity, UserInformationUiData>,
+) : ViewModel() {
+    private val _userInfos = MutableLiveData<ScreenState<UserInformationUiData>>()
+    val userInfos: LiveData<ScreenState<UserInformationUiData>> get() = _userInfos
+
+    init {
+        getUserInfosFromFirebase()
+    }
+
+    private fun getUserInfosFromFirebase() {
+        _userInfos.value = ScreenState.Loading
+        viewModelScope.launch {
+            readFirebaseUserInfosUseCase.invoke(
+                getUserIdFromSharedPref(sharedPreferences),
+                onSuccess = {
+                    _userInfos.postValue(ScreenState.Success(firebaseUserInfoToUiData.map(it)))
+                },
+            ) {
+                _userInfos.postValue(ScreenState.Error(it))
+            }
+        }
+    }
+}
